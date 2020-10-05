963c94135b0de662995779c890bbf9e179f1efb4
Daisugi <volble124@gmail.com>
2020-10-05 23:17:54 +0900

    add state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,10 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+  state = {
+
+  }
+  
   render () {
     return (
       <Aux>
