d8d599bab6577075c5c2f63a0347f05ae15b6e96
Daisugi <volble124@gmail.com>
2020-10-14 18:14:54 +0900

    modified sidedrawer

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,13 +4,14 @@ import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return ( 
-        <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>);
-    });
+
+  const ingredientSummary = Object.keys(this.props.ingredients)
+  .map(igKey => {
+    return ( 
+      <li key={igKey}>
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+      </li>);
+  });
 
   return (
     <Aux>
@@ -19,12 +20,13 @@ const orderSummary = props => {
       <ul>
         {ingredientSummary}
       </ul>
-  <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
     </Aux>
-  )
+
+  );
 };
 
 export default orderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../DrawerToggle/DrawerToggle';
 import classes from './Toolbar.css';
 
-const toolbar = propx => (
+const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked}/>
     <div className={classes.Logo}>
       <Logo />
     </div>
