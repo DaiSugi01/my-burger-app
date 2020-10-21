4a138447775330652e3dfecc7376a346a595ea2e
Daisugi <volble124@gmail.com>
2020-10-21 14:33:35 +0900

    add checkout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import checkoutSummary from './components/Order/CheckoutSummary/CheckoutSummary';
+import Checkout from './containers/Checkout/Checkout';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
